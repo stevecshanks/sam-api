7b825a6cccafd2d66956be8e244103a7d4d7346f
Steve Shanks <steve.c.shanks@gmail.com>
2021-01-04 22:23:04 +0000

    Greet user by email

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,12 +1,24 @@
 import json
+import pytest
 
 from hello_world import app
+from unittest.mock import Mock
 
 
-def test_lambda_handler():
-    response = app.lambda_handler({}, "")
+@pytest.fixture
+def event():
+    return {"requestContext": {"authorizer": {"claims": {"email": "an@example.com"}}}}
+
+
+@pytest.fixture
+def context():
+    return Mock()
+
+
+def test_lambda_handler(event, context):
+    response = app.lambda_handler(event, context)
 
     assert response["statusCode"] == 200
 
     data = json.loads(response["body"])
-    assert data["message"] == "Hello, Unknown Person!"
+    assert data["message"] == "Hello, an@example.com!"
