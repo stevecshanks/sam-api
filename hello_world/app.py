8a84890de65626df0c0a72bf32a2c6315c7bed3a
Steve Shanks <steve.c.shanks@gmail.com>
2021-01-08 10:18:48 +0000

    Greet user by full name

--- hello_world/app.py
+++ hello_world/app.py
@@ -13,20 +13,20 @@ def lambda_handler(event: Event, _: object):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": f"Hello, {user.email}!",
+                "message": f"Hello, {user.name}!",
             }
         ),
     }
 
 
 class User(NamedTuple):
-    email: str
+    name: str
 
     @staticmethod
     def from_event(event: Event) -> "User":
         authorizer = event["requestContext"].get("authorizer")
         if not authorizer:
             # SAM does not support Cognito when running locally
-            return User(email="unknown@example.com")
+            return User(name="Local Host")
 
-        return User(email=authorizer["claims"]["email"])
+        return User(name=authorizer["claims"]["name"])
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -7,7 +7,12 @@ from unittest.mock import Mock
 
 @pytest.fixture
 def event():
-    return {"requestContext": {"authorizer": {"claims": {"email": "an@example.com"}}}}
+    return {"requestContext": {"authorizer": {"claims": {"name": "An Example"}}}}
+
+
+@pytest.fixture
+def localhost_event():
+    return {"requestContext": {}}
 
 
 @pytest.fixture
@@ -15,10 +20,19 @@ def context():
     return Mock()
 
 
-def test_lambda_handler(event, context):
+def test_lambda_handler_returns_user_name(event, context):
     response = app.lambda_handler(event, context)
 
     assert response["statusCode"] == 200
 
     data = json.loads(response["body"])
-    assert data["message"] == "Hello, an@example.com!"
+    assert data["message"] == "Hello, An Example!"
+
+
+def test_lambda_handler_returns_user_name_on_localhost(localhost_event, context):
+    response = app.lambda_handler(localhost_event, context)
+
+    assert response["statusCode"] == 200
+
+    data = json.loads(response["body"])
+    assert data["message"] == "Hello, Local Host!"
