c0cf6f12c516249c2ae8cdea9491692812348c59
Steve Shanks <steve.c.shanks@gmail.com>
2021-01-11 21:48:15 +0000

    Add Prettier for linting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello, World!
-        </p>
+        <p>Hello, World!</p>
       </header>
     </div>
   );
