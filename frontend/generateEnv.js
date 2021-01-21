180b4dfedb4f7b68e5389d6b839d971f3d524f61
Steve Shanks <steve.c.shanks@gmail.com>
2021-01-21 23:30:10 +0000

    Generate .env file from stack outputs

---
+++ frontend/generateEnv.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const outputs = readStackOutputsFromStdin();
+writeEnvFile({
+  REACT_APP_AWS_COGNITO_USER_POOL_ID: outputs["CognitoUserPoolId"],
+  REACT_APP_AWS_COGNITO_APP_CLIENT_ID: outputs["CognitoClientId"],
+});
+
+function readStackOutputsFromStdin() {
+  const data = fs.readFileSync("/dev/stdin", "utf-8");
+  const outputs = JSON.parse(data.toString());
+
+  return outputs.reduce((values, output) => {
+    values[output["OutputKey"]] = output["OutputValue"];
+    return values;
+  }, {});
+}
+
+function writeEnvFile(envVariables) {
+  const data = Object.keys(envVariables)
+    .map((key) => `${key}=${envVariables[key]}\n`)
+    .join("");
+  fs.writeFileSync(".env", data);
+}
